2bd0d12cd95067072fc465698573fd87106b4539
Hannah Proctor <proctorhm22@gcc.edu>
2025-12-25 02:40:05 -0500

    README / just making sure i have a version with these comments

--- python_sol/main.py
+++ python_sol/main.py
@@ -1,27 +1,65 @@
 """
 Docstring for main
+
+Assumes three towers
+
+Game won when all stacked in tower three
 """
 
-# TODO: verbosity
+# TODO: verbosity / error handling
 
 from hanoi import Hanoi
 
 def run_game(num_disks: int) -> None:
     try:
         h = Hanoi(num_disks)
-        print(h)
-        h.swap(1, 2)
-        print(h)
-        h.swap(1, 3)
-        print(h)
-        h.swap(2, 3)
-        print(h)
-        h.swap(1, 2)
-        print(h)
-        h.swap(2, 3)
-        print(h)
+        # print(h)
+        # print(get_tower_max(h, 1))
+        # print(get_tower_max(h, 2))
+        # print(get_tower_max(h, 3))
+
+        # h.swap(1, 2)
+        # print(h)
+        # print(get_tower_max(h, 1))
+        # print(get_tower_max(h, 2))
+        # print(get_tower_max(h, 3))
+
+        # h.swap(1, 3)
+        # print(h)
+        # print(get_tower_max(h, 1))
+        # print(get_tower_max(h, 2))
+        # print(get_tower_max(h, 3))
+
+        # h.swap(2, 3)
+        # print(h)
+        # print(get_tower_max(h, 1))
+        # print(get_tower_max(h, 2))
+        # print(get_tower_max(h, 3))
+
+        # h.swap(1, 2)
+        # print(h)
+        # print(get_tower_max(h, 1))
+        # print(get_tower_max(h, 2))
+        # print(get_tower_max(h, 3))
+
+        # h.swap(2, 3)
+        # print(h)
     except Exception as e:
         print(e)
+
+def next_count(h: Hanoi):
+    """
+    Go down tower 1
+    """
+    pass
+
+def get_tower_max(h: Hanoi, i: int):
+    """
+    Return max disk in tower_i
+    """
+    if i < 1 or i > h.NUM_TOWERS:
+        raise ValueError(f"ERROR: invalid i -- {i} (must be between 1 and {h.NUM_TOWERS})")
+    return max(h.towers[i-1], default=-1)
 
 def main():
     num_disks = 3
